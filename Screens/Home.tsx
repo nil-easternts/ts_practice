27c9fa1590b9ec835633b1e279884fd56f2bfe07
nil.easternts <nil.easternts@gmail.com>
2024-10-29 12:40:26 +0530

    Ts in state & hook

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,5 +1,5 @@
-import { View, Text, Button } from 'react-native';
-import React from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
+import React, { useRef, useState } from 'react';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../AppNavigation/AppNavigator';
 import { useRoute } from '@react-navigation/native';
@@ -7,8 +7,27 @@ import { useRoute } from '@react-navigation/native';
 interface HomeProps {
     navigation: StackNavigationProp<RootStackParamList, 'Home'>;
 }
-
+interface User{
+    name:string,
+    age:number
+}
+interface Item{
+    name:string,
+    age:number
+}
 const Home = ({ navigation }: HomeProps) => {
+const [counter,setCounter]=useState<number>(0)
+const [name,setName]=useState<string>('')
+const [isLoad,setIsLoad]=useState<boolean>(false)
+const [user,setUser]=useState<User>({
+name:'',
+age:0
+})
+
+const [items,setItems]=useState<Item[]>([])
+const [details,setDetails]=useState<null>(null)
+
+const ref=useRef<TextInput>(null)
    
     const handleNavigate = () => {
         navigation.navigate('Chats',{
@@ -21,6 +40,7 @@ const Home = ({ navigation }: HomeProps) => {
         <View>
             <Text style={{ color: 'red' }}>Homes</Text>
             <Button title="Go to Chat Screen" onPress={handleNavigate} />
+            <TextInput ref={ref} />
         </View>
     );
 };
