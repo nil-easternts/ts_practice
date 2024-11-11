8c8631885ca1526d519ba59bc9c534610db66080
Nil Gandhi <nil.easternts@gmail.com>
2024-11-11 11:23:22 +0530

    temp changes

--- redux/notesSlice.ts
+++ redux/notesSlice.ts
@@ -1,20 +1,20 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import  { RootState } from "./store";
-export interface Note{
-title:string,
-desc:string
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {RootState} from './store';
+export interface Note {
+  title: string;
+  desc: string;
 }
-const initialState:Array<Note>=[]
-export const notesSlice=createSlice({
-    name:'notes',
-initialState,
-reducers:{
-    addNote(state,action:PayloadAction<Note>){
-        state.push(action.payload)
-    }
-}
-})
+const initialState: Array<Note> = [];
+export const notesSlice = createSlice({
+  name: 'notes',
+  initialState,
+  reducers: {
+    addNote(state, action: PayloadAction<Note>) {
+      state.push(action.payload);
+    },
+  },
+});
 
-export const {addNote}=notesSlice.actions
-export const noteSelector=(state:RootState)=>state.notes
-export  default notesSlice.reducer
+export const {addNote} = notesSlice.actions;
+export const noteSelector = (state: RootState) => state.notes;
+export default notesSlice.reducer;
